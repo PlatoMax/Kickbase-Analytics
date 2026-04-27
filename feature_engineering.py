e05cd82b97a91cff54861a53632a2f367eac19b4
PlatoMax <platomaximilian@gmail.com>
2026-04-27 18:02:42 +0200

    feat: Features points_trend, minuntes_avg_last_3, minutes_avg_last_5, minutes_trend
    Trend für Punkte, Durchschnitt und Trend Spielzeit

--- feature_engineering.py
+++ feature_engineering.py
@@ -12,7 +12,9 @@ conn.close()
 
 df = df.sort_values(by=['player_id', 'season', 'matchday']).reset_index(drop=True)   
     
-    
+
+# Punkte avg letzten 3 und 5 Spiele
+df["points_filled"] = df['points'].fillna(0)
 
 df["points_avg_last_3"] = (
     df.groupby(["player_id", "season"])["points_filled"] 
@@ -29,6 +31,31 @@ df["points_avg_last_5"] = (
 )
 
 df = df.drop(columns=["points_filled"])
+
+# Trend Punkte
+df["points_trend"] = df["points_avg_last_3"] - df["points_avg_last_5"]
+
+# Spielzeit avg letzten 3 und 5 Spiele
+df["minutes_filled"] = df["minutes"].fillna(0)
+
+df["minutes_avg_last_3"] = (
+    df.groupby(["player_id", "season"])["minutes_filled"]
+    .shift(1)
+    .rolling(window=3)
+    .mean()
+)
+
+df["minutes_avg_last_5"] = (
+    df.groupby(["player_id", "season"])["minutes_filled"]
+    .shift(1)
+    .rolling(window=5)
+    .mean()
+)
+
+# Trend Spielzeit
+df["minutes_trend"] = df["minutes_avg_last_3"] - df["minutes_avg_last_5"]
+
+df = df.drop(columns=["minutes_filled"])
 
 
 
