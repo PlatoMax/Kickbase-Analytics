3e30b83c6d97f567ce95239b5244526f0dd1f560
PlatoMax <platomaximilian@gmail.com>
2026-04-29 21:21:54 +0200

    refactor: feature engineering pipeline

--- feature_engineering.py
+++ feature_engineering.py
@@ -6,74 +6,167 @@ import numpy as np
 # Player_stats_field
 conn = sqlite3.connect("kickbase.db")
 
-query = "SELECT * FROM player_stats_field"
+query_field_players = "SELECT * FROM player_stats_field"
+query_goalkeeper = "SELECT * FROM player_stats_gk"
 
-df = pd.read_sql_query(query, conn)
+df_field_players = pd.read_sql_query(query_field_players, conn)
+df_goalkeeper = pd.read_sql_query(query_goalkeeper, conn)
 
 conn.close()
 
-df = df.sort_values(by=['player_id', 'season', 'matchday']).reset_index(drop=True)   
-    
+df_field_players = df_field_players.sort_values(by=['player_id', 'season', 'matchday']).reset_index(drop=True)   
+df_goalkeeper = df_goalkeeper.sort_values(by=['player_id', 'season', 'matchday']).reset_index(drop=True)
 
-# Punkte avg letzten 3 und 5 Spiele
-df["points_filled"] = df['points'].fillna(0)
+# Punkte avg und trend letzten 3 und 5 Spiele
+def points_avg_and_trend(df):
+    df["points_filled"] = df['points'].fillna(0)
 
-df["points_avg_last_3"] = (
+    df["points_avg_last_3"] = (
     df.groupby(["player_id", "season"])["points_filled"] 
     .shift(1)
     .rolling(window=3)
     .mean()
 )
-
-df["points_avg_last_5"] = (
+    df["points_avg_last_5"] = (
     df.groupby(["player_id", "season"])["points_filled"]
     .shift(1)
     .rolling(window=5)
     .mean()
 )
-
-df = df.drop(columns=["points_filled"])
-
-# Trend Punkte
-df["points_trend"] = df["points_avg_last_3"] - df["points_avg_last_5"]
+    df["points_trend"] = df["points_avg_last_3"] - df["points_avg_last_5"]
+    df = df.drop(columns=["points_filled"])
+    return df
 
 # Spielzeit avg letzten 3 und 5 Spiele
-df["minutes_filled"] = df["minutes"].fillna(0)
-
-df["minutes_avg_last_3"] = (
+def minutes_avg_and_trend(df):
+    df["minutes_filled"] = df["minutes"].fillna(0)
+    df["minutes_avg_last_3"] = (
     df.groupby(["player_id", "season"])["minutes_filled"]
     .shift(1)
     .rolling(window=3)
     .mean()
 )
-
-df["minutes_avg_last_5"] = (
+    df["minutes_avg_last_5"] = (
     df.groupby(["player_id", "season"])["minutes_filled"]
     .shift(1)
     .rolling(window=5)
     .mean()
 )
-
-# Trend Spielzeit
-df["minutes_trend"] = df["minutes_avg_last_3"] - df["minutes_avg_last_5"]
-
-df = df.drop(columns=["minutes_filled"])
+    df = df.drop(columns=["minutes_filled"])
+    return df
 
 
-df["points_p90_last_5"] = (
-    (df["points_avg_last_5"] / df["minutes_avg_last_5"]) * 90
-)
 
-df["points_p90_last_3"] = (
+def points_p90(df):
+    df["points_p90_last_5"] = (
+        (df["points_avg_last_5"] / df["minutes_avg_last_5"]) * 90
+    )
+    df["points_p90_last_5"] = df["points_p90_last_5"].replace([np.inf, -np.inf], np.nan).fillna(0)  
+
+    df["points_p90_last_3"] = (
     (df["points_avg_last_3"] / df["minutes_avg_last_3"]) * 90
 )
+    df["points_p90_last_3"] = df["points_p90_last_3"].replace([np.inf, -np.inf], np.nan).fillna(0)  
 
-df["points_p90_last_5"] = df["points_p90_last_5"].replace([np.inf, -np.inf], np.nan).fillna(0)
-df["points_p90_last_3"] = df["points_p90_last_3"].replace([np.inf, -np.inf], np.nan).fillna(0)
-
+    return df
 
 # Effizient messen in dem man den avg. * 90 rechnet
-efficiency_cols = [
+
+def p90(df, efficiency_cols):
+
+    for col in efficiency_cols:
+        df[f"{col}_filled"] = df[col].fillna(0)
+
+        df[f"{col}_avg_last_3"] = (
+            df.groupby(["player_id", "season"])[f"{col}_filled"]
+            .shift(1)
+            .rolling(window=3)
+            .mean()
+        )
+
+        df[f"{col}_p90_last_3"] = df[f"{col}_avg_last_3"] / df["minutes_avg_last_3"] * 90
+        df[f"{col}_p90_last_3"] = df[f"{col}_p90_last_3"].replace([np.inf, -np.inf], np.nan).fillna(0)  
+        
+        df = df.drop(columns=[f"{col}_filled", f"{col}_avg_last_3"])
+    
+    return df
+
+
+
+# Wahrscheinlichkeitsmetriken 
+def ratios(df, ratio_pairs):
+    for succes_col, total_col in ratio_pairs:
+        df[f"{succes_col}_filled"] = df[succes_col].fillna(0)
+        df[f"{total_col}_filled"] = df[total_col].fillna(0)
+
+        df[f"{succes_col}_sum_last_3"] = (
+        df.groupby(["player_id", "season"])[f"{succes_col}_filled"]        
+        .shift(1)
+        .rolling(window=3)
+        .sum()
+        )
+
+        df[f"{total_col}_sum_last_3"] = (
+        df.groupby(["player_id", "season"])[f"{total_col}_filled"]        
+        .shift(1)
+        .rolling(window=3)
+        .sum()
+        )
+
+        df[f"{succes_col}_ratio_last_3"] = df[f"{succes_col}_sum_last_3"] / df[f"{total_col}_sum_last_3"]
+        df[f"{succes_col}_ratio_last_3"] = df[f"{succes_col}_ratio_last_3"].replace([np.inf, -np.inf], np.nan).fillna(0)
+
+        df = df.drop(columns=[
+            f"{succes_col}_filled",
+            f"{succes_col}_sum_last_3",
+            f"{total_col}_filled",
+            f"{total_col}_sum_last_3"
+        ])
+
+    return df
+
+# Form von Spieler und Team berechnen
+
+
+def form_trends(df, form_trend_cols):
+    for col in form_trend_cols:
+        df[f"{col}_filled"] = df[col].fillna(0)
+        
+        df[f"{col}_avg_last_3"] = (
+            df.groupby(["player_id", "season"])[f"{col}_filled"]
+            .shift(1).rolling(window=3).mean()
+        )
+        
+        df[f"{col}_avg_last_5"] = (
+            df.groupby(["player_id", "season"])[f"{col}_filled"]
+            .shift(1).rolling(window=5).mean()
+        )
+        
+        df[f"{col}_trend"] = df[f"{col}_avg_last_3"] - df[f"{col}_avg_last_5"]
+        
+        df = df.drop(columns=[f"{col}_filled"])
+
+    return df
+
+# Summe für Goals, Assits und Karten
+def sums_cards_goals_assists(df):
+    df["cards_total"] = df["yellow_cards"].fillna(0) + df["yellow_red_cards"].fillna(0) * 2 + df["red_cards"].fillna(0) * 3
+
+    sum_cols = ["goals", "assists", "cards_total"]
+
+    for col in sum_cols:
+        df[f"{col}_sum_last_5"] = (
+            df.groupby(["player_id", "season"])[col]
+            .shift(1).rolling(window=5).sum().fillna(0)
+        )
+        
+    df = df.drop(columns=["cards_total"])
+
+    return df
+
+
+
+efficiency_cols_field = [
     "gewonnene_zweikaempfe",
     "schuesse_aufs_tor",
     "torschuss_vorlagen",
@@ -87,25 +180,7 @@ efficiency_cols = [
     "ballverluste"
 ]
 
-for col in efficiency_cols:
-    df[f"{col}_filled"] = df[col].fillna(0)
-
-    df[f"{col}_avg_last_3"] = (
-        df.groupby(["player_id", "season"])[f"{col}_filled"]
-        .shift(1)
-        .rolling(window=3)
-        .mean()
-    )
-
-    df[f"{col}_p90_last_3"] = df[f"{col}_avg_last_3"] / df["minutes_avg_last_3"] * 90
-    df[f"{col}_p90_last_3"] = df[f"{col}_p90_last_3"].replace([np.inf, -np.inf], np.nan).fillna(0)  
-    
-    df = df.drop(columns=[f"{col}_filled", f"{col}_avg_last_3"])
-
-
-
-# Wahrscheinlichkeitsmetriken 
-ratio_pairs = [
+ratio_pairs_field = [
     ("erfolgreiche_paesse", "paesse_gesamt"), 
     ("gewonnene_zweikaempfe", "gewonnene_zweikaempfe_gesamt"),
     ("gewonnene_luftkaempfe", "gewonnene_luftkaempfe_gesamt"),
@@ -114,70 +189,23 @@ ratio_pairs = [
     ("schussgenauigkeit", "schussgenauigkeit_gesamt")
 ]
 
-for succes_col, total_col in ratio_pairs:
-    df[f"{succes_col}_filled"] = df[succes_col].fillna(0)
-    df[f"{total_col}_filled"] = df[total_col].fillna(0)
 
-    df[f"{succes_col}_sum_last_3"] = (
-    df.groupby(["player_id", "season"])[f"{succes_col}_filled"]        
-    .shift(1)
-    .rolling(window=3)
-    .sum()
-    )
-
-    df[f"{total_col}_sum_last_3"] = (
-    df.groupby(["player_id", "season"])[f"{total_col}_filled"]        
-    .shift(1)
-    .rolling(window=3)
-    .sum()
-    )
-
-    df[f"{succes_col}_ratio_last_3"] = df[f"{succes_col}_sum_last_3"] / df[f"{total_col}_sum_last_3"]
-    df[f"{succes_col}_ratio_last_3"] = df[f"{succes_col}_ratio_last_3"].replace([np.inf, -np.inf], np.nan).fillna(0)
-
-    df = df.drop(columns=[
-        f"{succes_col}_filled",
-        f"{succes_col}_sum_last_3",
-        f"{total_col}_filled",
-        f"{total_col}_sum_last_3"
-    ])
-
-# Form von Spieler und Team berechnen
 form_trend_cols = [
     "goals_own_team", 
     "goals_enemy_team", 
     "match_result", 
-    "grade"             
+    "grade",
+    "points_per_value"            
 ]
 
-for col in form_trend_cols:
-    df[f"{col}_filled"] = df[col].fillna(0)
-    
-    df[f"{col}_avg_last_3"] = (
-        df.groupby(["player_id", "season"])[f"{col}_filled"]
-        .shift(1).rolling(window=3).mean()
-    )
-    
-    df[f"{col}_avg_last_5"] = (
-        df.groupby(["player_id", "season"])[f"{col}_filled"]
-        .shift(1).rolling(window=5).mean()
-    )
-    
-    df[f"{col}_trend"] = df[f"{col}_avg_last_3"] - df[f"{col}_avg_last_5"]
-    
-    df = df.drop(columns=[f"{col}_filled"])
 
-# Summe für Goals, Assits und Karten
-df["cards_total"] = df["yellow_cards"].fillna(0) + df["yellow_red_cards"].fillna(0) * 2 + df["red_cards"].fillna(0) * 3
-sum_cols = ["goals", "assists", "cards_total"]
-
-for col in sum_cols:
-    df[f"{col}_sum_last_5"] = (
-        df.groupby(["player_id", "season"])[col]
-        .shift(1).rolling(window=5).sum().fillna(0)
-    )
-    
-df = df.drop(columns=["cards_total"])
+df_field_players = points_avg_and_trend(df_field_players)
+df_field_players = minutes_avg_and_trend(df_field_players)
+df_field_players = points_p90(df_field_players)
+df_field_players = p90(df_field_players, efficiency_cols_field)
+df_field_players = ratios(df_field_players, ratio_pairs_field)
+df_field_players = form_trends(df_field_players, form_trend_cols)
+df_field_players = sums_cards_goals_assists(df_field_players)
 
 #---------------------------------------------------------------------------------------------------------------------------------
 # Playerstats Goalkeeper
